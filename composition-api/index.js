2f529cb7eacc2f5db10e2f4887980b0c7664e6fe
JACK LIN <cvse00566@gmail.com>
2025-04-17 21:52:10 +0800

    first commit
    get gitignore

---
+++ composition-api/index.js
@@ -0,0 +1,4 @@
+import { MousePosition } from "@/composition-api/tools/useMousePosition.js";
+
+export const useMousePosition = MousePosition;
+
---
+++ layouts/default.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
+
+<template>
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li><nuxt-link to="/">Home</nuxt-link></li>
+            <li><nuxt-link to="/about">About</nuxt-link></li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Nuxt />
+      </main>
+      <footer>
+        <p>&copy; 2023 Your Company</p>
+      </footer>
+    </div>
+  </template>
+
+<style scoped>
+header {
+  background-color: #333;
+  color: white;
+  padding: 1rem;
+}
+
+nav ul {
+  list-style: none;
+  display: flex;
+  gap: 1rem;
+}
+
+nav a {
+  color: white;
+  text-decoration: none;
+}
+
+footer {
+  text-align: center;
+  margin-top: 2rem;
+  padding: 1rem;
+  background-color: #f4f4f4;
+}
+</style>
